925c5ec6c6648ebd0ea96760653539e809bb1a2d
Ai Makabi <a.macbee@gmail.com>
2014-01-07 03:28:08 +0900

    基本的な環境用のテストファイルを追加

---
+++ env_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe service('iptables') do
+  it { should be_disable }
+  it { should be_stopped }
+end
+
+describe package('zsh') do
+  it { should be_installed }
+end
+
+describe command('echo $SHELL') do
+  it { should return_stdout '/bin/zsh' }
+end
+
+describe file('/home/vagrant/.zshrc') do
+  it { should be_file }
+  it { should contain 'alias sort="LC_ALL=C sort"' }
+  it { should contain 'alias uniq="LC_ALL=C uniq"' }
+  it { should contain 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"/usr/local/lib"' }
+  it { should contain 'export PATH=$PATH:"/usr/local/bin"' }
+end
+
+describe package('emacs') do
+  it { should be_installed }
+end
+
+describe yumrepo('epel') do
+  it { should exist }
+  it { should be_enabled }
+end
+
+describe package('tmux') do
+  it { should be_installed }
+end
