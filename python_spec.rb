be6f90eb8a760dbf8664c527ab1a1f04277dd028
Ai Makabi <a.macbee@gmail.com>
2014-01-07 03:26:28 +0900

    Python 用のテストファイルを追加

---
+++ python_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe file('/usr/bin/python') do
+  it { should be_file }
+  it { should be_executable }
+end
+
+describe file('/usr/bin/pip') do
+  it { should be_file }
+  it { should be_executable }
+end
+
+describe command('python -c "import numpy"') do
+  it { should return_exit_status 0 }
+end
+
+describe command('python -c "import scipy"') do
+  it { should return_exit_status 0 }
+end
+
+describe command('python -c "import matplotlib"') do
+  it { should return_exit_status 0 }
+end
