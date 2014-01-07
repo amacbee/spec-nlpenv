a7f5b90a34e77a211420982574723951fc1ce493
Ai Makabi <a.macbee@gmail.com>
2014-01-07 18:42:43 +0900

    iptables に関するテストを削除

--- env_spec.rb
+++ env_spec.rb
@@ -1,10 +1,5 @@
 require 'spec_helper'
 
-describe service('iptables') do
-  it { should be_disable }
-  it { should be_stopped }
-end
-
 describe package('zsh') do
   it { should be_installed }
 end
@@ -17,6 +12,7 @@ describe file('/home/vagrant/.zshrc') do
   it { should be_file }
   it { should contain 'alias sort="LC_ALL=C sort"' }
   it { should contain 'alias uniq="LC_ALL=C uniq"' }
+  it { should contain 'export LANG=ja_JP.UTF-8' }
   it { should contain 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"/usr/local/lib"' }
   it { should contain 'export PATH=$PATH:"/usr/local/bin"' }
 end
@@ -31,5 +27,9 @@ describe yumrepo('epel') do
 end
 
 describe package('tmux') do
+  it { should be_installed }
+end
+
+describe package('git') do
   it { should be_installed }
 end
